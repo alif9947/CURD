c9cfaa5da7788ab7b3de4d61336ec9239e71cc5c
alif <madbyhe@gmail.com>
2024-08-13 09:33:32 +0700

    update

--- src/pages/Dhasboard/Dhasboard.jsx
+++ src/pages/Dhasboard/Dhasboard.jsx
@@ -8,13 +8,12 @@ function Dhasboard() {
         <div>
           <Navbar />
 
-          <div className="flex flex-col md:px-10 px-3 py-5 w-full h-full ">
+          <div className="flex flex-col md:px-10 px-3 py-5 w-full h-full gap-5">
             <div className="flex items-center justify-between pt-5 gap-5 ">
-              <div className="h-[100px] w-[180px] md:h-[150px] md:w-[250px] bg-lime-700">
-                d
-              </div>
-              <div className="h-[100px] w-[180px] md:h-[150px] md:w-[250px] bg-lime-700"></div>
+              <div className="h-[100px] w-[180px] md:h-[150px] md:w-[250px] bg-lime-700 rounded-lg"></div>
+              <div className="h-[100px] w-[180px] md:h-[150px] md:w-[250px] bg-lime-700 rounded-lg"></div>
             </div>
+            <div className="flex justify-center">p</div>
           </div>
         </div>
         <div>
